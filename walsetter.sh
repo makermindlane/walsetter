7d583eb03e57c71c210299444344fdd120f67ac6
Pawan <makermindlane@gmail.com>
2022-06-24 22:31:15 +0530

    Initial commit

---
+++ walsetter.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/zsh
+
+file_path=$1
+
+# Get filename without extension
+filename=$(basename $file_path | sed 's/\(.*\)\..*/\1/')
+
+# Get directory
+dir_loc=$(dirname $file_path)
+
+# Check if file exists or not
+if [ ! -f "$file_path" ]; then
+	echo "$file_path does not exist"
+	exit 1
+else
+	echo "$file_path found."
+fi
+
+# Call pywal to generate color schemes
+wal -i $file_path
+
+# Set wallpaper
+~/.local/src/walsetter/ksetwallpaper.py -f $file_path
+
+# Move color scheme file to .local/share/color-schemes
+~/.local/src/walsetter/move-colors-kde.sh $filename
+
